b831c668911600f046c7df80c94ae67c5ad5103e
Marek Mateusz Narozniak <marek@aftership.net>
2016-06-21 17:58:21 +0800

    post buisness team review fixes

--- Api/Trackings.php
+++ Api/Trackings.php
@@ -24,10 +24,10 @@ class Trackings extends Http implements TrackingsInterface, AppInterface
 		$db->getProfiler()->setEnabled(true);
 		$select = $db->select();
 		$select->from(array('address' => 'sales_order_address'), array('country_id', 'telephone', 'postcode'));
-		$select->join(array('sorder' => 'sales_order'), 'address.parent_id = sorder.entity_id', array('entity_id', 'customer_note', 'customer_firstname', 'customer_middlename', 'customer_lastname', 'customer_email'));
-		$select->join(array('track' => 'sales_shipment_track'), 'sorder.entity_id = track.parent_id', array('track_number', 'title', 'carrier_code', 'CONVERT_TZ(`track`.`created_at`, @@session.time_zone, \'+00:00\') AS track_created_at'));
-		$select->join(array('item' => 'sales_order_item'), 'sorder.entity_id = item.order_id', array('GROUP_CONCAT(item.name SEPARATOR \'<>\') AS order_items'));
-		$select->where("CONVERT_TZ(`track`.`created_at`, @@session.time_zone, '+00:00') between FROM_UNIXTIME($from) and FROM_UNIXTIME($to)");
+		$select->join(array('sorder' => 'sales_order'), 'address.parent_id = sorder.entity_id', array('increment_id', 'customer_note', 'customer_firstname', 'customer_middlename', 'customer_lastname', 'customer_email'));
+		$select->join(array('track' => 'sales_shipment_track'), 'sorder.entity_id = track.order_id', array('track_number', 'title', 'carrier_code', 'CONVERT_TZ(`track`.`created_at`, @@session.time_zone, \'+00:00\') AS track_created_at'));
+		$select->join(array('item' => 'sales_order_item'), 'sorder.entity_id = item.order_id', array('GROUP_CONCAT(item.name SEPARATOR \'<>\') AS order_items', 'GROUP_CONCAT(FLOOR(item.qty_shipped) SEPARATOR \'<>\') AS order_quantities'));
+		$select->where("CONVERT_TZ(`track`.`created_at`, @@session.time_zone, '+00:00') between FROM_UNIXTIME($from) and FROM_UNIXTIME($to) and item.qty_shipped > 0");
 		if ($store >= 0) {
 			$select->where("sorder.store_id = $store");
 		}
